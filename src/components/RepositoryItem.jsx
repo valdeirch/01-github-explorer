e183d6bf845a5aad48708d669a6f5f5487ab9b75
valdeirsbs <valdeirsbs@gmail.com>
2021-12-02 18:19:32 -0300

    first commit

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,13 @@
+export function RepositoryItem({repository}) {
+  return (
+    <li>
+      <strong>
+        {repository?.name}
+      </strong>
+      <p>
+        {repository?.description}
+      </p>
+      <a href={repository?.link}>Acessar repositório</a>
+    </li>
+  )
+}
